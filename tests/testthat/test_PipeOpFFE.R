e727cf4eecb7ee27f8b1cd9b580f1402489bcca4
Sebastian Fischer <sebf.fischer@gmail.com>
2022-05-09 11:44:36 +0200

    test: Fix test after restructuring

--- tests/testthat/test_PipeOpFFE.R
+++ tests/testthat/test_PipeOpFFE.R
@@ -5,21 +5,19 @@ test_that("PipeOpFFE works", {
   task = tsk("fuel")
   pop = PipeOpFFE$new()
 
-  expect_error({pop$param_set$values$extractors = list()})
-
-  pop$param_set$values$extractors = list(mean = extract_mean)
+  pop$param_set$values$extractors = list(mean = extractor_mean())
   pop$param_set$values$affect_columns = mlr3pipelines::selector_name("xyz")
   x = pop$train(list(task))[[1L]]
   expect_set_equal(x$feature_names, task$feature_names)
 
   pop$param_set$values$affect_columns = mlr3pipelines::selector_all()
 
-  pop$param_set$values$extractors = list(mean = extract_mean)
+  pop$param_set$values$extractors = list(mean = extractor_mean())
   x = pop$train(list(task))[[1L]]
   expect_set_equal(x$feature_names, c("NIR.mean", "UVVIS.mean", "h20"))
 
   pop$param_set$values$affect_columns = mlr3pipelines::selector_name("NIR")
-  pop$param_set$values$extractors = list(mean = extract_mean, slope = extract_slope)
+  pop$param_set$values$extractors = list(mean = extractor_mean(), slope = extractor_slope())
   x = pop$train(list(task))[[1L]]
   expect_set_equal(x$feature_names, c("NIR.mean", "NIR.slope", "UVVIS", "h20"))
 
@@ -27,7 +25,7 @@ test_that("PipeOpFFE works", {
   x = pop$train(list(task))[[1L]]
   expect_set_equal(x$feature_names, c("NIR.mean", "UVVIS.mean", "UVVIS.slope", "NIR.slope", "h20"))
 
-
-
-
+  pop$param_set$values$affect_columns = mlr3pipelines::selector_all()
+  x = pop$train(list(task))[[1L]]
+  expect_set_equal(x$feature_names, c("NIR.mean", "UVVIS.mean", "UVVIS.slope", "NIR.slope", "h20"))
 })
--- tests/testthat/test_PipeOpFlatFunct.R
+++ tests/testthat/test_PipeOpFlatFunct.R
@@ -10,4 +10,22 @@ test_that("PipeOpFlatFunct works", {
   )
   expect_set_equal(x$feature_names, expected_features)
 
+  pop$param_set$values$affect_columns = selector_name("UVVIS")
+  x = pop$train(list(task))[[1L]]
+  expected_features = c(
+    "h20",
+    paste0("UVVIS.", 1:134),
+    "NIR"
+  )
+  expect_set_equal(x$feature_names, expected_features)
+
+  pop$param_set$values$affect_columns = selector_name("..xyz")
+  x = pop$train(list(task))[[1L]]
+  expected_features = c(
+    "h20",
+    "UVVIS",
+    "NIR"
+  )
+  expect_set_equal(x$feature_names, expected_features)
 })
+
