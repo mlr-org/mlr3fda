2f8f53da461914884483a7540d30e7d84c707e9f
Maximilian Mücke <muecke.maxilian@gmail.com>
2023-07-10 12:27:53 +0200

    fix: formatting

--- inst/bench/profile.R
+++ inst/bench/profile.R
@@ -6,10 +6,10 @@ library(mlr3pipelines)
 task <- tsk("fuel")
 po_fmean <- po("ffs", feature = "mean", id = "mean", drop = FALSE)
 po_fvar <- po("ffs", feature = "var", id = "var", drop = FALSE)
-po_slope <- po("ffs", feature = "slope", id = "slope", drop = FALSE)
+po_fslope <- po("ffs", feature = "slope", id = "slope", drop = FALSE)
 graph <- po_fmean %>>%
   po_fvar %>>%
-  po_slope
+  po_fslope
 
 profvis({
   graph$train(task)
