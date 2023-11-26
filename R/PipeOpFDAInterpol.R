f22f48a087936a4e120e05df271f345f1d3b6d66
Maximilian Muecke <muecke.maximilian@gmail.com>
2023-11-26 20:02:40 +0100

    docs: added docs for method param in interpol pipeop

--- R/PipeOpFDAInterpol.R
+++ R/PipeOpFDAInterpol.R
@@ -4,7 +4,7 @@
 #' @description
 #' Interpolate functional features (e.g. all individuals are observed at different time-points) to a common grid.
 #' This is useful if you want to compare functional features across observations.
-#' The interpolation is done using the `tf` package. See [`tf_interpolate()`][tf::tfd] for details.
+#' The interpolation is done using the `tf` package. See [`tfd()`][tf::tfd] for details.
 #'
 #' @section Parameters:
 #' The parameters are the parameters inherited from [`PipeOpTaskPreprocSimple`], as well as the following
@@ -18,8 +18,7 @@
 #'   The grid to use for interpolation.
 #'   If `grid` is numeric, it must be a sequence of values to use for the grid or a single value that
 #'   specifies the number of points to use for the grid, requires `left` and `right` to be specified.
-#'   If `grid` is a character, it must be either `"union"`, `"intersect"` or `"minmax"`.
-#'   For regular functional data this has no effect.
+#'   If `grid` is a character, it must be one of:
 #'   * `"union"`: This option creates a grid based on the union of all argument points from the provided functional
 #'     features. This means that if the argument points across features are \(t_1, t_2, ..., t_n\), then the grid will
 #'     be the combined unique set of these points. This option is generally used when the argument points vary across
@@ -28,8 +27,15 @@
 #'   * `"minmax"`: This option constructs a grid that spans from the maximum of the minimum argument points to the
 #'     minimum of the maximum argument points across the provided functional features. It creates a bounded grid that
 #'     encapsulates the range within which all features have defined argument points.
+#'   For regular functional data this has no effect.
 #'   Initial value is `"union"`.
-#' * `evaluator` :: `character(1)` \cr
+#' * `method` :: `character(1)` \cr
+#'   Defaults to `"linear"`. One of:
+#'   * `"linear"`: applies linear interpolation without extrapolation (see `tf::tf_approx_linear()`).
+#'   * `"spline"`: applies cubic spline interpolation (see `tf::tf_approx_spline()`).
+#'   * `"fill_extend"`: applies linear interpolation with constant extrapolation (see `tf::tf_approx_fill_extend()`).
+#'   * `"locf"`: applies "last observation carried forward" interpolation (see `tf::tf_approx_locf()`).
+#'   * `"nocb"`: applies "next observation carried backward" interpolation (see `tf::tf_approx_nocb()`).
 #' * `left` :: `numeric()` \cr
 #'   The left boundary of the window.
 #'   The window is specified such that the all values >=left and <=right are kept for the computations.
@@ -62,7 +68,7 @@ PipeOpFDAInterpol = R6Class("PipeOpFDAInterpol",
           }
           "Must be either a string or numeric vector."
         })),
-        evaluator = p_fct(
+        method = p_fct(
           c("linear", "spline", "fill_extend", "locf", "nocb"), default = "linear", tags = c("train", "predict")
         ),
         left = p_dbl(tags = c("train", "predict")),
@@ -83,15 +89,15 @@ PipeOpFDAInterpol = R6Class("PipeOpFDAInterpol",
     .transform_dt = function(dt, levels) {
       pars = self$param_set$get_values()
       grid = pars$grid
-      evaluator = pars$evaluator
+      method = pars$method
       left = pars$left
       right = pars$right
       if (!is.null(left) && !is.null(right)) {
         assert_count(grid)
         assert_true(left <= right)
       }
-      evaluator = evaluator %??% "linear"
-      evaluator = sprintf("tf_approx_%s", evaluator)
+      method = method %??% "linear"
+      evaluator = sprintf("tf_approx_%s", method)
       dt[, (names(dt)) := lapply(.SD, function(x) interpolate_col(x, grid, evaluator, left, right))][]
     }
   )
