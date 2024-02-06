16d8569320fbc964fffbbc957bc636b00d81a2d7
Sebastian Fischer <sebf.fischer@gmail.com>
2024-02-06 13:43:07 +0100

    test: pofu detects real duplicates of tfd

--- tests/testthat/test_tf.R
+++ tests/testthat/test_tf.R
@@ -8,3 +8,21 @@ test_that("tf does not support NAs", {
   x = invisible(tfd(d, arg = "time", value = "value", id = "id"))
   expect_true(length(x) == 0)
 })
+
+test_that("pofu has no surprises: irreg", {
+  # pofu can successfully determine that the two cca columns are identical
+  task = tsk("dti")$select("cca")
+  task1 = task$clone(deep = TRUE)
+  task1$id = "test"
+  taskout = po("featureunion")$train(list(task, task1))[[1L]]
+  expect_permutation(taskout$feature_names, "cca")
+})
+
+test_that("pofu has no surprises: reg", {
+  # pofu can successfully determine that the two cca columns are identical
+  task = tsk("fuel")$select("NIR")
+  task1 = task$clone(deep = TRUE)
+  task1$id = "test"
+  taskout = po("featureunion")$train(list(task, task1))[[1L]]
+  expect_permutation(taskout$feature_names, "NIR")
+})
