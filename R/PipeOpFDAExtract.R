02fca725b3181a6fcf6960822af5498cb00a9b25
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-25 00:07:55 +0200

    refactor: move helpers back since only used in extract

--- R/PipeOpFDAExtract.R
+++ R/PipeOpFDAExtract.R
@@ -216,6 +216,34 @@ make_fextractor = function(features) {
   }
 }
 
+transform_list = function(x) {
+  x = transpose_list(x)
+  map(x, unlist)
+}
+
+ffind = function(x, left = -Inf, right = Inf) {
+  n = length(x)
+  if (left <= x[[1L]] && right >= x[[n]]) {
+    return(c(1L, n))
+  }
+  if (left > x[[n]] || right < x[[1L]]) {
+    return(rep(NA_integer_, 2L))
+  }
+  it = findInterval(c(left, right), x)
+  # in case there are no values in the interval, it contains the index of the smallest value below
+  # and both values in it are identical,
+  # e.g. searching the interval (1.1, 1.2) in c(1, 2) returns c(1, 1) which we here convert to an NA interval
+  if (it[[1L]] == it[[2L]] && left > x[[it[[1L]]]]) {
+    return(rep(NA_integer_, 2L))
+  }
+  if (it[[1L]] == 0L) {
+    it[[1L]] = 1L
+  } else if (x[[it[[1L]]]] < left) {
+    it[[1L]] = it[[1L]] + 1L
+  }
+  it
+}
+
 fmean = function(arg, value) mean(value, na.rm = TRUE)
 fmin = function(arg, value) min(value, na.rm = TRUE)
 fmax = function(arg, value) max(value, na.rm = TRUE)
