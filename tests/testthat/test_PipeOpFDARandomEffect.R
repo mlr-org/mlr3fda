e1ba3caaee3a17203fe8cc63a32939aa5f542662
markusgoeswein <markus.goeswein@outlook.de>
2025-03-28 17:56:33 +0100

    draft of fda.randomeffect tests

---
+++ tests/testthat/test_PipeOpFDARandomEffect.R
@@ -0,0 +1,53 @@
+test_that("PipeOpFDARandomEffect - basic properties", {
+  pop = po("fda.randomeffect")
+  expect_pipeop(pop)
+  expect_identical(pop$id, "fda.randomeffect")
+})
+
+test_that("PipeOpFDARandomEffect works", {
+  # tf_reg works
+  task = tsk("fuel")$select("NIR")
+  po_fre = po("fda.randomeffect", drop = TRUE)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, c("NIR_random_intercept", "NIR_random_slope"))
+  
+  # drop = FALSE
+  po_fre = po("fda.randomeffect", drop = FALSE)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, c("NIR", "NIR_random_intercept", "NIR_random_slope"))
+  
+  # tf_irreg works
+  dt = data.table(
+    id = c("Ann", "Ann", "Ann", "Bob", "Bob"),
+    arg = c(1, 7, 2, 3, 5),
+    value = c(1, 2, 3, 4, 5)
+  )
+  f = tf::tfd(dt, id = "id", arg = "arg", value = "value")
+  y = c(1, 2)
+  dt = data.table(f = f, y = y)
+  task = as_task_regr(dt, target = "y")
+
+  po_fre = po("fda.randomeffect", drop = TRUE)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, c("f_random_intercept", "f_random_slope"))
+  
+  # drop = FALSE 
+  po_fre = po("fda.randomeffect", drop = FALSE)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, c("f", "f_random_intercept", "f_random_slope"))
+  
+  # re returns NA if no data in interval 
+  po_fre = po("fda.randomeffect", drop = TRUE, left = 100, right = 200)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, c("f_random_intercept", "f_random_slope"))
+  fre = task_fre$data()[, c('f_random_intercept', "f_random_slope")]
+  expect_true(all(is.na(unlist(fre))))
+  
+  # NAs for subjects with no data and random effects where values are present
+  task = tsk("dti")$select(c("cca", "rcst"))
+  po_fre = po("fda.randomeffect", drop = TRUE, left = 0.1, right = 0.2)
+  task_fre = train_pipeop(po_fre, list(task))[[1L]]
+  expect_set_equal(task_fre$feature_names, as.vector(outer(task$feature_names, c("random_intercept", "random_slope"), paste, sep = '_')))
+  expect_true(all(is.na(task_fre$data()[3, c("rcst_random_intercept", "rcst_random_slope")])))
+  
+})
