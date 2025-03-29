f28e309411b8448d3a65989254abade5d05beeef
Maximilian Muecke <muecke.maximilian@gmail.com>
2025-03-29 13:03:26 +0100

    more

--- tests/testthat/test_PipeOpFDAWavelets.R
+++ tests/testthat/test_PipeOpFDAWavelets.R
@@ -3,3 +3,13 @@ test_that("PipeOpFDAWavelets - basic properties", {
   expect_pipeop(pop)
   expect_identical(pop$id, "fda.wavelets")
 })
+
+test_that("PipeOpFDAWavelets works", {
+  task = tsk("fuel")
+  pop = po("fda.wavelets")
+  task_wav = train_pipeop(pop, list(task))[[1L]]
+  new_data = task_wav$data()
+  expect_task(task_wav)
+  expect_identical(dim(new_data), c(129L, 34L))
+  expect_named(new_data, names(new_data))
+})
