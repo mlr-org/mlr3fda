5e6204d0d3a3c21325a71eda4402c30b31209eef
Maximilian Muecke <muecke.maximilian@gmail.com>
2026-07-09 16:31:16 +0200

    test: fix for upcoming tf change

--- tests/testthat/test_tf.R
+++ tests/testthat/test_tf.R
@@ -1,12 +1,21 @@
-test_that("tf does not support NAs", {
-  # Various PipeOps assume that there are no functional NAs
-  # This test will inform us whether this feature is implemented,
-  # in which case we then have to adress this case in the PipeOps
-  # https://github.com/tidyfun/tf/issues/33
-  # Currently, NA functions are dropped
-  d = data.frame(time = 1, value = NA_real_, id = "1", stringsAsFactors = FALSE)
-  x = tf::tfd(d, arg = "time", value = "value", id = "id")
-  expect_length(x, 0L)
+test_that("tf all-NA functional input", {
+  # Canary for how tf handles all-NA functional input.
+  # Historically tf::tfd() silently collapsed all-NA input to length 0 (dropping data);
+  # a later tf release preserves vec_size() and returns a length-n vector of NA functions
+  # with a warning instead (tidyfun/tf#33, tidyfun/tf#241, tidyfun/tf#267).
+  #
+  # Either way, mlr3fda's feature-extraction PipeOps do not support NA functions: like
+  # mlr3pipelines::PipeOpPCA on missing values, they may error. NA functions must be
+  # imputed or dropped upstream (e.g. na.omit the backing data) before feature extraction.
+  dt = data.table(time = 1, value = NA_real_, id = "1")
+  if (packageVersion("tf") <= "0.4.1") {
+    x = tf::tfd(dt, arg = "time", value = "value", id = "id")
+    expect_length(x, 0L)
+  } else {
+    expect_warning(x <- tf::tfd(d, arg = "time", value = "value", id = "id"))
+    expect_length(x, 1L)
+    expect_true(allMissing(x))
+  }
 })
 
 test_that("pofu has no surprises: irreg", {
